47b6be2b6bca27408caa7606a884b7009408c03d
Jorge Galeas <jorge.galeas@mutualistapichincha.com>
2026-02-22 16:31:40 -0500

    first commit

--- docs/guestData.js
+++ docs/guestData.js
@@ -1,11 +1,11 @@
 // guestData.js
 // Lista de invitados como objeto JavaScript
 const guestList = {
-    'INV001': { names: 'Alex G', seats: 1, city: 'QUITO', shortCity: 'UIO' },
-    'INV002': { names: 'Maria A y Esposo', seats: 2, city: 'AMBATO', shortCity: 'AMB' },
-    'INV003': { names: 'Ana PÃƒÂ©rez', seats: 2, city: 'LATACUNGA', shortCity: 'LTGA' },
-    'INV004': { names: 'Luis GÃƒÂ³mez y Marta', seats: 3, city: 'LATACUNGA', shortCity: 'LTGA' },
-    'INV005': { names: 'Familia Torres', seats: 4, city: 'LATACUNGA', shortCity: 'LTGA' },
-    'INV006': { names: 'Carlos', seats: 1, city: 'QUITO', shortCity: 'UIO' }
-    // Agrega mÃƒÂ¡s invitados aquÃƒÂ­
+    'INV001': { names: 'Alex G', nombre: 'Alex G', celular: '', seats: 1, city: 'QUITO', shortCity: 'UIO' },
+    'INV002': { names: 'Maria A y Esposo', nombre: 'Maria A y Esposo', celular: '', seats: 2, city: 'AMBATO', shortCity: 'AMB' },
+    'INV003': { names: 'Ana PÃƒÆ’Ã‚Â©rez', nombre: 'Ana PÃƒÆ’Ã‚Â©rez', celular: '', seats: 2, city: 'LATACUNGA', shortCity: 'LTGA' },
+    'INV004': { names: 'Luis GÃƒÆ’Ã‚Â³mez y Marta', nombre: 'Luis GÃƒÆ’Ã‚Â³mez y Marta', celular: '', seats: 3, city: 'LATACUNGA', shortCity: 'LTGA' },
+    'INV005': { names: 'Familia Torres', nombre: 'Familia Torres', celular: '', seats: 4, city: 'LATACUNGA', shortCity: 'LTGA' },
+    'INV006': { names: 'Carlos', nombre: 'Carlos', celular: '', seats: 1, city: 'QUITO', shortCity: 'UIO' }
+    // Agrega mÃƒÆ’Ã‚Â¡s invitados aquÃƒÆ’Ã‚Â­
 };
